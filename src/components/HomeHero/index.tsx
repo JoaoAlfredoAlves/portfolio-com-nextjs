be331e1d0f476d9960184ad73533895d8aeba138
João Alfredo Alves <68473607+JoaoAlfredoAlves@users.noreply.github.com>
2022-04-05 19:11:25 -0300

    Criando e Estilizando a HomeHero

---
+++ src/components/HomeHero/index.tsx
@@ -0,0 +1,42 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
+import { Container, TextContainer, InfosContainer, CodeItem } from './styles';
+import picture from '../../assets/pessoa.png';
+
+function HomeHero() {
+    return (
+        <Container>
+            <img src={picture} alt="Minha Foto" />
+            <div>
+                <TextContainer>
+                    <h1>Olá</h1>
+                    <h2>Sou o João Alfredo</h2>
+                </TextContainer>
+                <InfosContainer>
+                    <CodeItem>
+                        <span className='comment'>// Minha Apresentação</span>
+                        <span className='purple'>Infos</span> {'\u007B'}
+                        <div>
+                            Nome: <span className='blue'>João,</span>
+                        </div>
+                        <div>
+                            Sobrenome: <span className='blue'>Alfredo</span>
+                        </div>
+                        {'\u007D'}
+                    </CodeItem>
+                    <CodeItem>
+                        <span className='purple'>Cargo</span> {'\u007B'}
+                        <div>
+                            Função: <span className='blue'>Desenvolvedor Web,</span>
+                        </div>
+                        <div>
+                            Area: <span className='blue'>Front-End</span>
+                        </div>
+                        {'\u007D'}
+                    </CodeItem>
+                </InfosContainer>
+            </div>
+        </Container>
+    );
+};
+
+export default HomeHero;
