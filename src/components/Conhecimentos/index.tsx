9662e34c8c2be98ede5bb86e7139616f3d2ff2d9
João Alfredo Alves <68473607+JoaoAlfredoAlves@users.noreply.github.com>
2022-04-07 22:29:23 -0300

    Estilizando a parte de conhecimentos
    Estilizando os conhecimentos e adcionando ( importando ) outros mais.

--- src/components/Conhecimentos/index.tsx
+++ src/components/Conhecimentos/index.tsx
@@ -1,6 +1,7 @@
 import { AiFillHtml5 } from "react-icons/ai";
 import { FaCss3Alt, FaReact } from "react-icons/fa";
-import { IoLogoJavascript } from "react-icons/io";
+import { DiNodejsSmall, DiGit, DiGithubBadge, DiMarkdown, DiVisualstudio, DiJsBadge, DiPython } from "react-icons/di";
+import { SiFigma } from "react-icons/si";
 import SectionTitle from '../SectionTitle';
 import ConhecimentoItem from "./ConhecimentoItem";
 import { Container } from './styles';
@@ -10,10 +11,17 @@ function Conhecimentos() {
     <Container>
       <SectionTitle title="Conhecimentos" />
       <section>
-        <ConhecimentoItem title="HTML" icon={<AiFillHtml5 />} />
-        <ConhecimentoItem title="CSS" icon={<FaCss3Alt />} />
-        <ConhecimentoItem title="JavaScript" icon={<IoLogoJavascript />} />
+        <ConhecimentoItem title="HTML 5" icon={<AiFillHtml5 />} />
+        <ConhecimentoItem title="CSS 3" icon={<FaCss3Alt />} />
+        <ConhecimentoItem title="Figma" icon={<SiFigma />} />
         <ConhecimentoItem title="React" icon={<FaReact />} />
+        <ConhecimentoItem title="JavaScript" icon={<DiJsBadge />} />
+        <ConhecimentoItem title="Node.js" icon={<DiNodejsSmall />} />
+        <ConhecimentoItem title="Git" icon={<DiGit />} />
+        <ConhecimentoItem title="GitHub" icon={<DiGithubBadge />} />
+        <ConhecimentoItem title="Markdown" icon={<DiMarkdown />} />
+        <ConhecimentoItem title="Visual Studio Code" icon={<DiVisualstudio />} />
+        <ConhecimentoItem title="Python" icon={<DiPython />} />
       </section>
     </Container>
   );
